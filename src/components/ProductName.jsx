e777665c7347adb3285d12081eca0bd90ad6f564
swathisunil18 <swathisunil220@gmail.com>
2025-02-25 22:44:28 +0530

    working

---
+++ src/components/ProductName.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const ProductName = () => {
+  return (
+    <div>
+       <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Product Name</label>
+                        <input type="text" className="form-control" />
+                    
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                   
+                    <label htmlFor="" className="form-label">Category</label>
+                    <input type="text" className="form-control" />
+
+
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                   
+                    <label htmlFor="" className="form-label">Price</label>
+                    <input type="text" className="form-control" />
+
+
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                   
+                    <label htmlFor="" className="form-label">Manufactoring Date</label>
+                    <input type="text" className="form-control" />
+
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                   
+                    <label htmlFor="" className="form-label">Expiry</label>
+                    <input type="text" className="form-control" />
+
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                   
+                    <label htmlFor="" className="form-label">Manufacturer Name</label>
+                    <input type="text" className="form-control" />
+
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+                     <button className="btn btn-success">Submit</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+       </div>
+
+
+
+    </div>
+  )
+}
+
+export default ProductName
