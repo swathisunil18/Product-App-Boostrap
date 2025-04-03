c5e49746f269efc31302598e91e6bebb0feb10d6
swathisunil18 <swathisunil220@gmail.com>
2025-04-03 20:29:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import ProductName from './components/ProductName';
 import SearchProduct from './components/SearchProduct';
+import ShoppingCard from './components/ShoppingCard';
+
 
 function App() {
   return (
     <div >
      <ProductName />
      <SearchProduct />
+     <ShoppingCard />
+    
+     
     </div>
   );
 }
--- src/components/ShoppingCard.jsx
+++ src/components/ShoppingCard.jsx
@@ -1,25 +1,211 @@
 import React from 'react'
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-    
 const ShoppingCard = () => {
-
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
-};
-   
+    <div>
 
+
+
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card" >
+  <img src="https://www.buyindusvalley.in/cdn/shop/files/Gentle-daily-Nourishing-shampoo.jpg?v=1733478515" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Shampoo</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card" >
+  <img src="https://magicaljar.com/wp-content/uploads/2022/07/magicaljar-kumkumadi-glowing-face-wash-1-1000x1000.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Face Wash</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+                    
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card" >
+  <img src="https://m.media-amazon.com/images/I/61zVBRnpO1L._AC_UF1000,1000_QL80_.jpghttps://www.buyindusvalley.in/cdn/shop/files/Gentle-daily-Nourishing-shampoo.jpg?v=1733478515" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Lipstick</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+                    
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+                <div class="card" >
+  <img src="https://m.media-amazon.com/images/I/71nGfxIFFLL._AC_UF1000,1000_QL80_.jpg://m.media-amazon.com/images/I/61klPW9ugJL._AC_UF1000,1000_QL80_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Serum</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+                    
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card" >
+  <img src="https://m.media-amazon.com/images/I/615CmmbRzGL._AC_UF1000,1000_QL80_.jpg://www.buyindusvalley.in/cdn/shop/files/Gentle-daily-Nourishing-shampoo.jpg?v=1733478515" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Hair Oil</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+                    
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+                <div class="card" >
+  <img src="https://m.media-amazon.com/images/I/61MujrQxciL._AC_UY1000_.jpg://www.buyindusvalley.in/https://www.tanishq.sg/on/demandware.static/-/Sites-Tanishq-site-catalog/default/dw63271186/images/hi-res/511178VKB2A00_1.jpg/shop/files/Gentle-daily-Nourishing-shampoo.jpg?v=1733478515" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Necklace</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+                    
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+                <div class="card" >
+  <img src="https://www.tanishq.sg/on/demandware.static/-/Sites-Tanishq-site-catalog/default/dw63271186/images/hi-res/511178VKB2A00_1.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Bangles</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+                    
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+                <div class="card" >
+  <img src="https://www.buyindusvalley.in/cdn/shop/files/Gentle-daily-Nourishing-shampoo.jpg?v=1733478515" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Shampoo</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+                    
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+
+                <div class="card" >
+  <img src="https://www.buyindusvalley.in/cdn/shop/files/Gentle-daily-Nourishing-shampoo.jpg?v=1733478515" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Shampoo</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+
+
+
+
+
+
+                    
+                </div>
+                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card" >
+  <img src="https://magicaljar.com/wp-content/uploads/2022/07/magicaljar-kumkumadi-glowing-face-wash-1-1000x1000.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Face Wash</h5>
+    <p class="card-text">Price : 120/-</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    </div>
+  )
+}
 
 export default ShoppingCard
